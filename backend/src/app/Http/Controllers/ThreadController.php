7bad329f9b53ca88112836da5e9a0db91578cbc4
ISAWASHUN <syun20000616@icloud.com>
2024-07-01 00:01:35 +0900

    スレッドAPIの作成

---
+++ backend/src/app/Http/Controllers/ThreadController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ThreadResource;
+use App\Http\Resources\ThreadWithResponseResource;
+use App\Models\Thread;
+use Illuminate\Http\Request;
+
+class ThreadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $threads = Thread::all();
+
+        return ThreadWithResponseResource::collection($threads);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $thread = Thread::create($request->only(
+            ['title', 'name', 'email', 'content']
+        ));
+
+        return new ThreadResource($thread);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id)
+    {
+        $thread = Thread::findOrFail($id);
+
+        return new ThreadWithResponseResource($thread);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Thread $thread)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Thread $thread)
+    {
+        //
+    }
+}
